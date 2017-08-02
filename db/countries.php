691b4c8fc7ef8ba08d4d9d73837faccf5804827d
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-08-02 00:48:03 -0400

    fix (countries now has id AI primary key)

--- db/countries.php
+++ db/countries.php
@@ -1,10 +1,13 @@
 <?php
   echo 'Creating Countries...<br>';
 
-  $sql = "CREATE TABLE IF NOT EXISTS Regions (
+  //TODO: Drop Table if Exists for seeding
+  $sql = "CREATE TABLE IF NOT EXISTS Countries (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name TEXT NOT NULL,
-    PRIMARY KEY (ID)
+    region_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (REGION_ID) references Regions(id)
   )";
 
   $db->query($sql);
