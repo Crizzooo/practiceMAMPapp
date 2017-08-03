a7a6d3f201d7175a724841946871bb7b7720b191
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-08-03 02:03:18 -0400

    feat (index.php now runs index.js in client side browser)

--- index.php
+++ index.php
@@ -1,37 +1,41 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <?php
-  /*
-    mysql:host=localhost;dbname=test;port=3306;charset=utf8
-  driver^^ colon    ^param=value pair         ^semicolon
+   <head>
+    <title>PHP Test</title>
+   </head>
+   <body>
+     <?php
+      /*
+        mysql:host=localhost;dbname=test;port=3306;charset=utf8
+      driver^^ colon    ^param=value pair         ^semicolon
 
-  */
-  $dsn = 'mysql:host=localhost';
-  $user = 'root';
-  $password ='root';
+      */
+      $dsn = 'mysql:host=localhost';
+      $user = 'root';
+      $password ='root';
 
-  $dbName = "FruitAnalysis";
-  try {
-    $db = new PDO($dsn, $user, $password);
-    // set the PDO error mode to exception
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $dbName = "FruitAnalysis";
+      try {
+        $db = new PDO($dsn, $user, $password);
+        // set the PDO error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "CREATE DATABASE IF NOT EXISTS $dbName";
-    $db->exec($sql);
-    if ($db->query("use $dbName")) {
-      echo "PDO connection to $dbName is successful!  <br>";
-    }
-  } catch (PDOException $e) {
-    echo 'Connection failed: '.$e->getMessage();
-  }
+        $sql = "CREATE DATABASE IF NOT EXISTS $dbName";
+        $db->exec($sql);
+        if ($db->query("use $dbName")) {
+          echo "PDO connection to $dbName is successful!  <br>";
+        }
+      } catch (PDOException $e) {
+        echo 'Connection failed: '.$e->getMessage();
+      }
 
 
-  // Seed Database
-  include './db/seed.php';
+      // Seed Database
+      include './db/seed.php';
 
-  echo 'connection closed.';
+      echo 'connection closed.';
 
- ?>
+     ?>
+     <script type="text/javascript" src="index.js"></script>
+   </body>
 </html>
